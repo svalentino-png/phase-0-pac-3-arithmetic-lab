61d3935b7dd3f5226b509f01b7046f33862e894a
Sam Valentino <sammyvalentino7@gmail.com>
2024-12-09 20:44:20 -0500

    completed arithmetic lab

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+    return "something";
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return "something";
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
